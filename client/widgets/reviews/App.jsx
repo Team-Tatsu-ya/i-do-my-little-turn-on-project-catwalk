e87f095d7a87f0927aac7310efb0cd912af32fe9
Daniel Leal <leadaniel456@gmail.com>
2020-09-23 16:44:29 -0500

    Add structure to review widget

---
+++ client/widgets/reviews/App.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+
+class Review extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+
+    //binding of methods
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  //get and post request
+
+  render() {
+
+    return (
+      <div>
+       This is where the reviews are suppose to be
+      </div>
+    )
+  }
+}
+
+export default Review;
