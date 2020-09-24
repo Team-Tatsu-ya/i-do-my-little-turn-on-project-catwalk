78bd1521b6b424fe6889e20ceaf0d266be88cbed
Daniel Leal <leadaniel456@gmail.com>
2020-09-24 16:12:20 -0500

    Pass props to ReviewList

--- client/widgets/reviews/App.jsx
+++ client/widgets/reviews/App.jsx
@@ -6,7 +6,7 @@ class Review extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      product = dummyData,
     }
 
     //binding of methods
@@ -24,6 +24,7 @@ class Review extends Component {
     return (
       <div>
        This is where the reviews are suppose to be
+       <ReviewList reviews={this.state.product}/>
       </div>
     )
   }
--- client/widgets/reviews/OneReview.jsx
+++ client/widgets/reviews/OneReview.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const OneReview = (props) => {
-
-}
+  return (
+    <div>
+      <Rating
+        name="simple-controlled"
+        value={props.individual.rating}
+        precision={0.5}
+        readOnly
+      />
+      {props.individual.summary}
+      {props.individual.date}
+      {props.individual.reviewer_name}
+      {props.individual.body}
+      {props.individual.response}
+    </div>
+  )
+};
 
 export default OneReview;
